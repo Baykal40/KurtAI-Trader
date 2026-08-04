34a7c44dfdffc8ad85616dbd38aa1e2e49cdc679
Baykal Kurt <habay40@hotmail.com>
2026-08-05 01:38:28 +0300

    v0.4.1 Environment Variables

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+from dotenv import load_dotenv
+import os
+
 from binance.client import Client
 
-client = Client()
+load_dotenv()
+
+api_key = os.getenv("BINANCE_API_KEY")
+api_secret = os.getenv("BINANCE_API_SECRET")
+
+client = Client(api_key, api_secret)
